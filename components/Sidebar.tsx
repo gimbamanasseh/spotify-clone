7832b5f784e46531a6fe225762f61b021955b7d8
Gimba Manasseh A <manasseh.gimba.a@gmail.com>
2023-11-29 17:15:11 +0100

    player functionality
    play, pause, next and prev song all based on the library or playlist you play from

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,8 +4,10 @@ import { usePathname } from "next/navigation";
 import { Component, useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import { twMerge } from "tailwind-merge";
 
 import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
 
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
@@ -18,6 +20,8 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
   const pathname = usePathname();
+  const player = usePlayer();
+
   const routes = useMemo(
     () => [
       {
@@ -36,7 +40,12 @@ const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
     [pathname]
   );
   return (
-    <div className="flex h-full">
+    <div
+      className={twMerge(
+        `flex h-full`,
+        player.activeId && "h-[calc(100%-80px)]"
+      )}
+    >
       {/* the sidebar */}
       <div className="hidden md:flex flex-col gap-y-w-2 bg-black h-full w-[300px] p-2">
         {/* sidebar - menu box */}
